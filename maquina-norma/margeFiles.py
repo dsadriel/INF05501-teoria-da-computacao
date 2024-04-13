f759d21323a341ec90564fa0338aa43a78692b5d
Adriel de S <ds.adriel@gmail.com>
2024-04-13 12:43:49 -0300

    reordenação dos diretórios e inclusão das questões 2 e 3

--- maquina-norma/margeFiles.py
+++ maquina-norma/margeFiles.py
@@ -4,28 +4,25 @@ import os
 operations_file = "C:\\Users\\dsadr\\Documents\\Github\\INF05501-teoria-da-computacao\\maquina-norma\\operations.mn"
 
 # Path to the trabalho folder
-parts_folder = "C:\\Users\\dsadr\\Documents\\Github\\INF05501-teoria-da-computacao\\maquina-norma\\trabalho\\parts"
+parts_folder = "C:\\Users\\dsadr\\Documents\\Github\\INF05501-teoria-da-computacao\\maquina-norma\\trabalho"
 
-destination_folder = "C:\\Users\\dsadr\\Documents\\Github\\INF05501-teoria-da-computacao\\maquina-norma\\trabalho"
+destination_folder = "C:\\Users\\dsadr\\Documents\\Github\\INF05501-teoria-da-computacao\\maquina-norma\\trabalho\\merged_files"
 
 # List all files in the trabalho folder
 files = os.listdir(parts_folder)
 
-# Open the operations.mn file in append mode
-with open(operations_file, "r") as operations:
-    # Iterate over each file in the trabalho folder
-    operations = operations.read()
+# Merge the files in parts_folder with operations_file
+for file in files:
+    # Read the content of the file in parts_folder
+    with open(os.path.join(parts_folder, file), 'r') as f:
+        content = f.read()
     
-    for file_name in files:
-        # Construct the full path to the file
-        file_path = os.path.join(parts_folder, file_name)
-        
-        # Open the file in read mode
-        with open(file_path, "r") as file:
-            # Read the contents of the file
-            contents =  operations + file.read()
-            
-            with open(destination_folder + "\\" + file_name, "w") as destination_file:
-                # Write the contents to the operations.mn file
-                destination_file.write(contents)
-            
+    # Append the content of operations_file to the content
+    with open(operations_file, 'r') as f:
+        operations_content = f.read()
+        content += operations_content
+    
+    # Write the merged content to the destination_folder
+    merged_file_path = os.path.join(destination_folder, file)
+    with open(merged_file_path, 'w') as f:
+        f.write(content)
